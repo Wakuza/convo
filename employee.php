502e27f224e2d3d18125819455a0bd4c545d37b3
Wakuza <pxy9548@rit.edu>
2015-04-21 15:42:26 -0400

    Removed Duplicated MySQL Query

--- employee.php
+++ employee.php
@@ -24,22 +24,17 @@
     </div><br/>
 
 <?php
-    /*
-    ***************** employees will log in and see the tables only if they are managers *****************
-    */
-
-    $query = "SELECT e.employeeID, e.firstname, e.lastname, e.position_name, CONCAT(s.firstname, ' ', s.lastname) AS supervisor, e.hire_date, e.review_date, e.employment_status FROM employee s RIGHT JOIN employee e ON e.supervisorID = s.employeeID where e.supervisorID = " . $user_data["employeeID"];
-    $result = mysqli_query($link, $query);
 
     /*
     * See every employees
     */
     if($user_data["admin_privileges"] == 1) {
-        $query = "SELECT e.employeeID, e.firstname, e.lastname, e.position_name, CONCAT(s.firstname, ' ', s.lastname) AS supervisor, e.hire_date, e.review_date, e.employment_status FROM employee s RIGHT JOIN employee e ON e.supervisorID = s.employeeID";
+        $query = "SELECT e.employeeID, e.firstname, e.lastname, e.position_name, CONCAT(s.firstname, ' ', s.lastname) AS supervisor, e.hire_date, e.payroll_status, e.employment_status FROM employee s RIGHT JOIN employee e ON e.supervisorID = s.employeeID";
         $result = mysqli_query($link, $query);
     }
+    // The managers only see the employees if they have their own supervisorID (name)
     else {
-        $query = "SELECT e.employeeID, e.firstname, e.lastname, e.position_name, CONCAT(s.firstname, ' ', s.lastname) AS supervisor, e.hire_date, e.review_date, e.employment_status FROM employee s RIGHT JOIN employee e ON e.supervisorID = s.employeeID where e.supervisorID = " . $user_data["employeeID"];
+        $query = "SELECT e.employeeID, e.firstname, e.lastname, e.position_name, CONCAT(s.firstname, ' ', s.lastname) AS supervisor, e.hire_date, e.payroll_status, e.employment_status FROM employee s RIGHT JOIN employee e ON e.supervisorID = s.employeeID where e.supervisorID = " . $user_data["employeeID"];
         $result = mysqli_query($link, $query);   
     }
 
@@ -47,9 +42,9 @@
 
     echo "<table id='example' class='display' cellspacing='0' width='1010px'>";
         if ($result && $num_rows > 0) { 
-           echo "<thead><tr><th>ID</th><th>First Name</th><th>Last Name</th><th style='text-align:center;'>Position</th><th>Supervisor</th><th>Hire Date</th><th>Review Date</th><th>Status</th></tr></thead><tbody>";
+           echo "<thead><tr><th>ID</th><th>First Name</th><th>Last Name</th><th style='text-align:center;'>Position</th><th>Supervisor</th><th>Hire Date</th><th>Payroll Status</th><th>Status</th></tr></thead><tbody>";
             while ($row = mysqli_fetch_assoc($result)) {
-                echo "<tr><td>" . $row["employeeID"] . "</td><td>" . $row["firstname"] . "</td><td>" . $row["lastname"] .  "</td><td>" . $row["position_name"] . "</td><td>" . $row["supervisor"] . "</td><td>" . $row["hire_date"] . "</td><td>" . $row["review_date"]. "</td><td>" . $row["employment_status"] . "</td></tr>";  
+                echo "<tr><td>" . $row["employeeID"] . "</td><td>" . $row["firstname"] . "</td><td>" . $row["lastname"] .  "</td><td>" . $row["position_name"] . "</td><td>" . $row["supervisor"] . "</td><td>" . $row["hire_date"] . "</td><td>" . $row["payroll_status"]. "</td><td>" . $row["employment_status"] . "</td></tr>";  
             }
         }        
     echo "</tbody></table>";
