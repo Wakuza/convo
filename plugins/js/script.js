7114a4c59e72f7e2f177641ba26ab3d827926fb7
Wakuza <pxy9548@rit.edu>
2015-04-22 15:09:55 -0400

    Added onload() for onchange

--- plugins/js/script.js
+++ plugins/js/script.js
@@ -12,6 +12,79 @@ var flag = false;
 function onLoad(){
     document.getElementById("showHide").innerHTML = "Show";
     document.getElementById("showHide").style.cursor = "pointer";
+    
+    
+        var empID = $("#employeeName").val().split("|")[0];
+        var pos = $("#employeeName").val().split("|")[1];
+        var payrollStatus = $("#employeeName").val().split("|")[2];
+        var department = $("#employeeName").val().split("|")[3];
+        var convo_location = $("#employeeName").val().split("|")[4];
+        var emp_status = $("#employeeName").val().split("|")[5];
+        var firstname = $("#employeeName").val().split("|")[6];
+        var lastname = $("#employeeName").val().split("|")[7];
+        var supervisor = $("#employeeName").val().split("|")[8];
+        var admin_privileges = $("#employeeName").val().split("|")[9];
+        var manager_privileges = $("#employeeName").val().split("|")[10];
+        var street_address = $("#employeeName").val().split("|")[11];
+        var city = $("#employeeName").val().split("|")[12];
+        var res_state = $("#employeeName").val().split("|")[13];
+        var zipCode = $("#employeeName").val().split("|")[14];
+        
+        // Employee Information
+        //if("select[name='employeeName'] option:selected"){
+        $("input[name='employeeID']").val(empID);
+        $("select[name='change_position_name']").val(pos);
+        $("input[name='current_position_name']").val(pos);
+        $("input[name='current_payroll_status']").val(payrollStatus);
+        $("select[name='change_payroll_status']").val(payrollStatus);
+        $("input[name='current_department']").val(department);
+        $("select[name='department']").val(department);
+        $("input[name='current_convo_location']").val(convo_location);
+        $("select[name='convo_location']").val(convo_location);
+        $("input[name='current_emp_status']").val(emp_status);
+        $("select[name='emp_status']").val(emp_status);
+        $("input[name='current_supervisor']").val(supervisor);
+        $("select[name='supervisor']").val(supervisor); 
+        $("input[name='current_admin_privileges']").val(admin_privileges);
+        if($("input[name='current_admin_privileges']").val() == "1") {
+          $("select[name='admin_privileges']").val("Admin");  
+        }
+        else {
+            $("select[name='admin_privileges']").val("Non_admin"); 
+        }
+        
+        
+        $("input[name='current_manager_privileges']").val(manager_privileges);
+        //$("select[name='manager_privileges']").val(manager_privileges);
+        
+        if($("input[name='current_manager_privileges']").val() == "1") {
+          $("select[name='manager_privileges']").val("Manager");  
+        }
+        else {
+            $("select[name='manager_privileges']").val("Non_manager"); 
+        }
+        
+        
+        // Personal Information
+        $("input[name='current_firstname']").val(firstname);
+        $("input[name='firstname']").val(firstname);
+        $("input[name='current_lastname']").val(lastname);
+        $("input[name='lastname']").val(lastname);
+        $("input[name='current_street_address']").val(street_address);
+        $("input[name='street_address']").val(street_address);
+        $("input[name='current_city']").val(city);
+        $("input[name='city']").val(city);
+        $("input[name='current_res_state']").val(res_state);
+        $("select[name='res_state']").val(res_state);
+        $("input[name='current_zipCode']").val(zipCode);
+        $("input[name='zipCode']").val(zipCode);
+    
+    
+    
+    
+    
+    
+    
 }
 
 function clicked(){  
@@ -60,74 +133,6 @@ $(document).ready(function() {
        else {  document.getElementById("termination_box").style.display = "none";
        }
    });
-
-    $("#employeeName").change(function() {
-            var empID = $(this).val().split("|")[0];
-            var pos = $(this).val().split("|")[1];
-            var payrollStatus = $(this).val().split("|")[2];
-            var department = $(this).val().split("|")[3];
-            var convo_location = $(this).val().split("|")[4];
-            var emp_status = $(this).val().split("|")[5];
-            var firstname = $(this).val().split("|")[6];
-            var lastname = $(this).val().split("|")[7];
-            var supervisor = $(this).val().split("|")[8];
-            var admin_privileges = $(this).val().split("|")[9];
-            var manager_privileges = $(this).val().split("|")[10];
-            var street_address = $(this).val().split("|")[11];
-            var city = $(this).val().split("|")[12];
-            var res_state = $(this).val().split("|")[13];
-            var zipCode = $(this).val().split("|")[14];
-
-            // Employee Information
-            $("input[name='employeeID']").val(empID);
-            $("select[name='change_position_name']").val(pos);
-            $("input[name='current_position_name']").val(pos);
-            $("input[name='current_payroll_status']").val(payrollStatus);
-            $("select[name='change_payroll_status']").val(payrollStatus);
-            $("input[name='current_department']").val(department);
-            $("select[name='department']").val(department);
-            $("input[name='current_convo_location']").val(convo_location);
-            $("select[name='convo_location']").val(convo_location);
-            $("input[name='current_emp_status']").val(emp_status);
-            $("select[name='emp_status']").val(emp_status);
-            $("input[name='current_supervisor']").val(supervisor);
-            $("select[name='supervisor']").val(supervisor); 
-            $("input[name='current_admin_privileges']").val(admin_privileges);
-            if($("input[name='current_admin_privileges']").val() == "1") {
-              $("select[name='admin_privileges']").val("Admin");  
-            }
-            else {
-                $("select[name='admin_privileges']").val("Non_admin"); 
-            }
-
-
-            $("input[name='current_manager_privileges']").val(manager_privileges);
-            //$("select[name='manager_privileges']").val(manager_privileges);
-
-            if($("input[name='current_manager_privileges']").val() == "1") {
-              $("select[name='manager_privileges']").val("Manager");  
-            }
-            else {
-                $("select[name='manager_privileges']").val("Non_manager"); 
-            }
-
-
-            // Personal Information
-            $("input[name='current_firstname']").val(firstname);
-            $("input[name='firstname']").val(firstname);
-            $("input[name='current_lastname']").val(lastname);
-            $("input[name='lastname']").val(lastname);
-            $("input[name='current_street_address']").val(street_address);
-            $("input[name='street_address']").val(street_address);
-            $("input[name='current_city']").val(city);
-            $("input[name='city']").val(city);
-            $("input[name='current_res_state']").val(res_state);
-            $("select[name='res_state']").val(res_state);
-            $("input[name='current_zipCode']").val(zipCode);
-            $("input[name='zipCode']").val(zipCode);
-
-            //$("input[name='emp_status']").setAttribute("selected", "selected");     
-        });
 });
     
     function textCounter( field, countfield, maxlimit ) {
