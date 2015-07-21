063803d0a5aa46506787d104611fe42e2930589f
Wakuza <pxy9548@rit.edu>
2015-07-21 12:20:50 -0400

    DB Connections

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,8 +1,8 @@
 <?php
     $db_host = "localhost";
     $db_user = "root";
-    $db_pass = "";
-    $db_name = "testing";
+    $db_pass = "mysql#1!";
+    $db_name = "convotesting";
   
     // Intialize the database connection
     $link = mysqli_connect ($db_host, $db_user, $db_pass, $db_name);
