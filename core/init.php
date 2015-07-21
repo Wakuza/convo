f76a4bc5905f78412bcf29016fd28336c08dbc8a
Wakuza <pxy9548@rit.edu>
2015-07-21 11:31:03 -0400

    Added root environment variables, breadcrumbs

--- core/init.php
+++ core/init.php
@@ -1,7 +1,7 @@
 <?php
-    $_ENV["HOSTNAME"] = "TESTING";
-    $linkToALL = "https://test.theinfini.com/convo";  
-    $root = realpath($_SERVER["DOCUMENT_ROOT"]);
+    $_ENV["HOSTNAME"] = "DEVELOPING";
+    $linkToALL = "/convo";  
+    $root = $_SERVER["DOCUMENT_ROOT"] . "/";
 
     session_start();
  
@@ -10,7 +10,8 @@
     require "functions/general.php";
 
     $current_file = explode("/", $_SERVER["SCRIPT_NAME"]);
-    $current_file = "/convo/HR/" . end($current_file);
+    $current_file_password = "/convo/" . end($current_file);
+    $current_file_employment = "/convo/HR/" . end($current_file);
 
     if(logged_in() === true) {
         $session_user_id = $_SESSION['employee_id'];
@@ -23,18 +24,18 @@
         $position_data = position_data($session_user_id, "employee_id", "job_code");
         if(user_active($user_data["username"]) === false) {
             session_destroy();
-            header("Location: index.php");
+            header("Location: $linkToALL/index.php");
             exit();
         }
 
         // This one forces the user to change the password when they click "forget the password"
-        if($current_file !== "changepassword.php" && $current_file !== "logout.php" && $user_data["password_recover"] == 1) {
-            header("Location: changepassword.php?force");
+        if($current_file_password !== "$linkToALL/changepassword.php" && $current_file !== "logout.php" && $user_data["password_recover"] == 1) {
+            header("Location: $linkToALL/changepassword.php?force");
             exit();
         }
         
         // If the employees don't have email, it is being forced to the employment_data.php and fill out the e-mail address
-        if($current_file !== "/convo/HR/employment_data.php" && $current_file !== "logout.php" && $user_data["email"] == NULL) {
+        if($current_file_employment !== "/convo/HR/employment_data.php" && $current_file !== "logout.php" && $user_data["email"] == NULL) {
             header("Location: $linkToALL/HR/employment_data.php?force");
             exit();
         }
